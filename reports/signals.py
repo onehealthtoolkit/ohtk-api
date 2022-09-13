667e158c3be49ddeeca7df20ff1232b90a53cd4f
pphetra <pphetra@gmail.com>
2022-09-13 20:42:51 +0700

    fix issue: "new report" websocket channel must support tenant model.

--- reports/signals.py
+++ reports/signals.py
@@ -8,6 +8,7 @@ from django.dispatch import receiver
 from accounts.models import Authority
 from reports.consumers import new_report_group_name
 from reports.models import IncidentReport
+from django.db import connection
 
 
 @receiver(
@@ -17,19 +18,15 @@ from reports.models import IncidentReport
 )
 def on_create_report(sender, instance, action, reverse, model, pk_set, **kwargs):
     if action == "post_add":
-        report_id = instance.id
+        schema_name = connection.schema_name
         for authority_id in pk_set:
             for authority in Authority.objects.get(pk=authority_id).all_inherits_up():
-                group_name = new_report_group_name(authority.id)
+                group_name = new_report_group_name(schema_name, authority.id)
                 channel_layer = channels.layers.get_channel_layer()
                 async_to_sync(channel_layer.group_send)(
                     group_name,
                     {
                         "type": "new.report",
-                        "text": json.dumps(
-                            {
-                                "report_id": report_id,
-                            }
-                        ),
+                        "text": json.dumps(instance.template_context()),
                     },
                 )
