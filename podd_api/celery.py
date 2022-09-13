b9b81b5942dc09298bd2234eda96b8bf59c6c795
pphetra <pphetra@gmail.com>
2022-09-13 17:58:13 +0700

    use tenant-schemas-celery

--- podd_api/celery.py
+++ podd_api/celery.py
@@ -1,11 +1,14 @@
 import os
 
 from celery import Celery
+from django.conf import settings
+from tenant_schemas_celery.app import CeleryApp as TenantAwareCeleryApp
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "podd_api.settings")
 
-app = Celery("podd-api")
+app = TenantAwareCeleryApp("podd-api")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
-app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
