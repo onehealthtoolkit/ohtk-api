52b5dc9af97321330ef53b63bc7ebba5a2d0429f
pphetra <pphetra@gmail.com>
2022-11-10 16:38:42 +0700

    fix bug: admin or officer role could delete notification.

--- cases/schema/mutations/admin_authority_notification_delete_mutation.py
+++ cases/schema/mutations/admin_authority_notification_delete_mutation.py
@@ -1,4 +1,5 @@
 import graphene
+from graphql import GraphQLError
 from graphql_jwt.decorators import login_required, superuser_required, user_passes_test
 
 from accounts.utils import is_officer_role, is_superuser, fn_or, is_admin_role
@@ -13,8 +14,18 @@ class AdminAuthorityNotificationDeleteMutation(graphene.Mutation):
 
     @staticmethod
     @login_required
-    @user_passes_test(fn_or(is_superuser, is_officer_role, is_admin_role))
     def mutate(root, info, id):
+        user = info.context.user
+
         definition = AuthorityNotification.objects.get(pk=id)
+        if user.is_superuser:
+            pass
+        elif user.is_authority_user:
+            if user.authority_role == "ADM" or user.authority_role == "OFF":
+                if definition.authority_id != user.authorityuser.authority_id:
+                    raise GraphQLError("Permission denied")
+        else:
+            raise GraphQLError("Permission denied")
+
         definition.delete(hard=True)
         return {"success": True}
