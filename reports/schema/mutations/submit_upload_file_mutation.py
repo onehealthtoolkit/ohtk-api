3ba18d8e9607e994bb64687724d8b991b3a4d237
pphetra <pphetra@gmail.com>
2023-06-22 13:50:38 +0700

    use magic.from_file

--- reports/schema/mutations/submit_upload_file_mutation.py
+++ reports/schema/mutations/submit_upload_file_mutation.py
@@ -24,7 +24,7 @@ class SubmitUploadFile(graphene.Mutation):
     @staticmethod
     @login_required
     def mutate(root, info, report_id, file, file_id):
-        file_type = magic.from_buffer(file.read(), mime=True)
+        file_type = magic.from_file(file)
         m = re.compile(r"(audio|video|application|text)")
 
         if m.match(file_type):
