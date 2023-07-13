8953b4afd27086ba438de68eeca5ccf3e2a92228
Polawat Phetra <pphetra@gmail.com>
2023-07-13 14:55:38 +0700

    docker compose for local testing.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,6 +8,17 @@ python manage.py collectstatic --noinput
 echo "Apply database migrations"
 python manage.py migrate
 
+# Create django superuser
+if [ -z "$DJANGO_SUPERUSER_USERNAME" ] || [ -z "$DJANGO_SUPERUSER_PASSWORD" ] || [ -z "$DJANGO_SUPERUSER_EMAIL" ]
+then
+    echo "No superuser created"
+else
+    echo "Creating superuser"
+    python manage.py createsuperuser --noinput \
+        --username $DJANGO_SUPERUSER_USERNAME \
+        --email $DJANGO_SUPERUSER_EMAIL
+fi
+
 # Start server
 echo "Starting server"
 python manage.py runserver 0.0.0.0:8000
