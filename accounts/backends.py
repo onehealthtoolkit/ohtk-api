5a3852ba254739e5801261516d36bcccfd0e968f
Polawat Phetra <pphetra@gmail.com>
2023-08-21 14:43:09 +0700

    Supports authenticate via OpenID Connect (OIDC).

--- accounts/backends.py
+++ accounts/backends.py
@@ -1,5 +1,6 @@
 from graphql_jwt.backends import JSONWebTokenBackend
 from graphql_jwt.exceptions import JSONWebTokenExpired
+from oauth2_provider.oauth2_validators import OAuth2Validator
 
 
 class MyJSONWebTokenBackend(JSONWebTokenBackend):
@@ -8,3 +9,22 @@ class MyJSONWebTokenBackend(JSONWebTokenBackend):
             return super().authenticate(request, **kwargs)
         except JSONWebTokenExpired:
             return None
+
+
+class CustomOAuth2Validator(OAuth2Validator):
+    def get_additional_claims(self, request):
+        return {
+            "username": request.user.username,
+            "user_id": request.user.id,
+            "email": request.user.email,
+        }
+
+    def get_userinfo_claims(self, request):
+        claims = super().get_userinfo_claims(request)
+        claims["sub"] = request.user.id
+        claims["username"] = request.user.username
+        claims["user_id"] = request.user.id
+        claims["email"] = request.user.email
+        claims["first_name"] = request.user.first_name
+        claims["last_name"] = request.user.last_name
+        return claims
