1b84952b5291fbec907c0625ecfb63f317884a88
Polawat Phetra <pphetra@gmail.com>
2023-12-21 12:39:06 +0700

    Add request to delete account mutation and update user fields
    This commit adds a new mutation for requesting to delete a user account. It also updates the user fields to remove personal information when the account is deactivated.

--- accounts/schema/mutations/request_to_delete_my_account_mutation.py
+++ accounts/schema/mutations/request_to_delete_my_account_mutation.py
@@ -9,9 +9,24 @@ class RequestToDeleteMyAccountMutation(graphene.Mutation):
     success = graphene.Boolean()
 
     @staticmethod
-    @login_required
     def mutate(root, info):
         user = info.context.user
+
+        if not user.is_authenticated:
+            return RequestToDeleteMyAccountMutation(success=False)
+
+        user.is_active = False
+        user.first_name = ""
+        user.last_name = ""
+        user.email = ""
+        user.save()
+
+        if user.is_authority_user:
+            user.authorityuser.avatar_url = None
+            user.authorityuser.thumbnail_avatar_url = None
+            user.authorityuser.telephone = None
+            user.authorityuser.save()
+
         success = True
 
         return RequestToDeleteMyAccountMutation(success=success)
