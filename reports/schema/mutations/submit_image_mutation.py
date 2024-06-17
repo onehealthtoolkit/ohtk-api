8ce80544a5a1ffe6e6a5fbe58df1677bc917f5f9
Polawat Phetra <pphetra@gmail.com>
2024-06-17 21:56:14 +0700

    quick fix: fix offline mobile bugs. that send observation image via report image endpoint

--- reports/schema/mutations/submit_image_mutation.py
+++ reports/schema/mutations/submit_image_mutation.py
@@ -1,4 +1,5 @@
 import graphene
+from django.db.models import Q
 from easy_thumbnails.files import get_thumbnailer
 from graphene_file_upload.scalars import Upload
 from graphql_jwt.decorators import login_required
@@ -26,13 +27,11 @@ class SubmitImage(graphene.Mutation):
     def mutate(root, info, report_id, image, is_cover, image_id):
 
         # temporary fix bug.
-        # current mobile device submit all images to this endpoint,
+        # currently mobile device submit all images to this endpoint,
         # so we need to check if the report_id is incident or followup or observation record, monitoring record
         report = None
         is_incident_report = False
         is_followup_report = False
-        is_subject_record = False
-        is_monitoring_record = False
         print("report_id", report_id)
         print("image_id", image_id)
 
@@ -49,24 +48,8 @@ class SubmitImage(graphene.Mutation):
             except FollowUpReport.DoesNotExist:
                 print("FollowUpReport.DoesNotExist")
 
-        if not report:
-            try:
-                report = SubjectRecord.objects.get(pk=report_id)
-                is_subject_record = True
-            except SubjectRecord.DoesNotExist:
-                print("SubjectRecord.DoesNotExist")
-
-        if not report:
-            try:
-                report = MonitoringRecord.objects.get(pk=report_id)
-                is_monitoring_record = True
-            except MonitoringRecord.DoesNotExist:
-                print("MonitoringRecord.DoesNotExist")
-
         print("is_incident_report", is_incident_report)
         print("is_followup_report", is_followup_report)
-        print("is_subject_record", is_subject_record)
-        print("is_monitoring_record", is_monitoring_record)
 
         if is_incident_report or is_followup_report:
             # check idempotent
@@ -94,8 +77,8 @@ class SubmitImage(graphene.Mutation):
                 thumbnail=get_thumbnailer(image.file)["thumbnail"].url,
                 image_url=image.file.url,
             )
-        elif is_subject_record or is_monitoring_record:
-
+        else:
+            # check idempotent
             if image_id and RecordImage.objects.filter(id=image_id).exists():
                 image = RecordImage.objects.get(id=image_id)
                 return SubmitImage(
@@ -105,21 +88,36 @@ class SubmitImage(graphene.Mutation):
                     image_url=image.file.url,
                 )
 
+            # lookup image_id in form_data of subject record
+            record = SubjectRecord.objects.filter(
+                Q(form_data__house_front__has_key=image_id)
+                | Q(form_data__consent_image__has_key=image_id)
+            ).first()
+
+            if not record:
+                record = MonitoringRecord.objects.filter(
+                    Q(form_data__deploy_image__has_key=image_id)
+                    | Q(form_data__indoor_container_neg__has_key=image_id)
+                    | Q(form_data__outdoor_container_neg__has_key=image_id)
+                    | Q(form_data__outdoor_container_pos__has_key=image_id)
+                    | Q(form_data__indoor_container_neg__value__has_key=image_id)
+                ).first()
+
+            if not record:
+                raise ValueError("Record not found.")
+
             image = RecordImage.objects.create(
-                record=report,
+                record=record,
                 file=image,
                 id=image_id,
             )
 
             if is_cover:
-                report.cover_image_id = image.id
-                report.save(update_fields=("cover_image_id"))
+                record.cover_image_id = image.id
+                record.save(update_fields=("cover_image_id"))
             return SubmitImage(
                 id=image.id,
                 file=image.file.url,
                 thumbnail=get_thumbnailer(image.file)["thumbnail"].url,
                 image_url=image.file.url,
             )
-
-        else:
-            raise ValueError("Not found")
